ca6f58ea0c419fe3476307c2660366954dfbbd2c
carlosgabrielcarreno <carrenocarlosgabriel@gmail.com>
2023-02-26 19:05:44 -0300

    add styleds main component

--- src/Components/BoxTracking/BoxTracking.jsx
+++ src/Components/BoxTracking/BoxTracking.jsx
@@ -1,5 +1,9 @@
 import "./_boxTracking.scss";
 
 export const BoxTracking = ({ nameClass }) => {
-  return <div className={`${nameClass}`}>BoxTracking</div>;
+  return (
+    <div className={`${nameClass}`}>
+      <div className="container-data-state"></div>
+    </div>
+  );
 };
--- src/Components/UserTracking/UserTracking.jsx
+++ src/Components/UserTracking/UserTracking.jsx
@@ -1,5 +1,21 @@
 import "./_userTracking.scss";
+import avatar from "../../../images/image-jeremy.png";
 
 export const UserTracking = () => {
-  return <main className="user">User</main>;
+  return (
+    <main className="container-main">
+      <div className="container-data-user">
+        <img className="image-profile" src={avatar} alt="image profile" />
+        <div className="container-name">
+          <p>Report for</p>
+          <h3>Jeremy Robson</h3>
+        </div>
+      </div>
+      <div className="container-states">
+        <button className="btn-state">Daily</button>
+        <button className="btn-state">Weekly</button>
+        <button className="btn-state">Monthly</button>
+      </div>
+    </main>
+  );
 };
