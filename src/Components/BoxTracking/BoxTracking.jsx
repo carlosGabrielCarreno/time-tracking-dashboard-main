fec840ce07c138068d8764cd8c40225a71525c6a
carlosgabrielcarreno <carrenocarlosgabriel@gmail.com>
2023-02-27 02:25:31 -0300

    working with grid css

--- src/Components/BoxTracking/BoxTracking.jsx
+++ src/Components/BoxTracking/BoxTracking.jsx
@@ -1,9 +1,15 @@
 import "./_boxTracking.scss";
+import ellipsis from "../../../images/icon-ellipsis.svg";
 
 export const BoxTracking = ({ nameClass }) => {
   return (
     <div className={`${nameClass}`}>
-      <div className="container-data-state"></div>
+      <div className="container-data-state">
+        <h3>{`${nameClass}`}</h3>
+        <h2>32hrs</h2>
+        <img src={ellipsis} alt="icon ellipsis" />
+        <p>last Week - 36hrs</p>
+      </div>
     </div>
   );
 };
