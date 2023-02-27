696295224fea6d9cdd59d60f0a78a0b82c521266
carlosgabrielcarreno <carrenocarlosgabriel@gmail.com>
2023-02-27 11:17:06 -0300

    add all styleds from box tracking components

--- src/Components/BoxTracking/BoxTracking.jsx
+++ src/Components/BoxTracking/BoxTracking.jsx
@@ -1,9 +1,13 @@
 import "./_boxTracking.scss";
 import ellipsis from "../../../images/icon-ellipsis.svg";
+import { getImageLogotype } from "../../utils/getImageLogotype";
 
 export const BoxTracking = ({ nameClass }) => {
   return (
     <div className={`${nameClass}`}>
+      <div className="container-logotype">
+        <img src={getImageLogotype(nameClass)} alt="" />
+      </div>
       <div className="container-data-state">
         <h3>{`${nameClass}`}</h3>
         <h2>32hrs</h2>
---
+++ src/utils/getImageLogotype.js
@@ -0,0 +1,26 @@
+import exercise from "../../images/icon-exercise.svg";
+import play from "../../images/icon-play.svg";
+import study from "../../images/icon-ellipsis.svg";
+import work from "../../images/icon-work.svg";
+import social from "../../images/icon-social.svg";
+import selfCare from "../../images/icon-self-care.svg";
+
+export const getImageLogotype = (name) => {
+  switch (name) {
+    case "work":
+      return work;
+    case "study":
+      return study;
+    case "play":
+      return play;
+    case "exercise":
+      return exercise;
+    case "social":
+      return social;
+    case "self-care":
+      return selfCare;
+
+    default:
+      break;
+  }
+};
