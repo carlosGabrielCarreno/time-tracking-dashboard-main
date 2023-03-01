44ae40ee71f5202ff3c461ae831b63c005fb2823
carlosgabrielcarreno <carrenocarlosgabriel@gmail.com>
2023-03-01 01:48:05 -0300

    add css grid in all projects

--- src/utils/getImageLogotype.js
+++ src/utils/getImageLogotype.js
@@ -1,6 +1,6 @@
 import exercise from "../../images/icon-exercise.svg";
 import play from "../../images/icon-play.svg";
-import study from "../../images/icon-ellipsis.svg";
+import study from "../../images/icon-study.svg";
 import work from "../../images/icon-work.svg";
 import social from "../../images/icon-social.svg";
 import selfCare from "../../images/icon-self-care.svg";
@@ -19,7 +19,6 @@ export const getImageLogotype = (name) => {
       return social;
     case "self-care":
       return selfCare;
-
     default:
       break;
   }
